e80998558f7c5f8ce90d056665cb7e87c6421ecd
J <78283807+qian-json@users.noreply.github.com>
2026-09-02 08:05:40 -0500

    chore: clean up API keys
    The url and anon key were already inert because the Supabase project has been down for a while but I removed them for good practice

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import {createClient} from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://orgynwndnrayqggharos.supabase.co";
-export const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yZ3lud25kbnJheXFnZ2hhcm9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcyODUzMjUsImV4cCI6MjAxMjg2MTMyNX0.wNBnDadc_fGGlei2WbbY8Q5ixifFcvUf6eqoEQOZ3wA";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
